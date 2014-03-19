75c14f44b7c043c251a6bee91f7d9e3bc7255f94
Rui Hu <code@mrhooray.com>
2014-03-19 14:52:47 -0700

    add tests

--- test/rpg.js
+++ test/rpg.js
@@ -15,7 +15,7 @@ describe('rpg.js', function () {
 
   describe('options', function () {
     it('should accept plain object as options', function () {
-      var options = [{}, {mode: 'lu', length: 8}];
+      var options = [{}, {set: 'lu', length: 8}];
       options.forEach(function (option) {
         (function () {
           rpg(option).should.be.type('string').and.not.be.empty;
@@ -34,7 +34,7 @@ describe('rpg.js', function () {
   });
 
   describe('options.length', function () {
-    it('should be able to configure password length', function () {
+    it('should be able to set password length', function () {
       var lengths = [1, 16, 1024];
       lengths.forEach(function (length) {
         rpg({length: length}).should.be.type('string').and.with.length(length);
@@ -60,13 +60,94 @@ describe('rpg.js', function () {
     });
   });
 
-  describe('options.mode', function () {
-    it.skip('', function () {
+  describe('options.set', function () {
+    it('should be able to set character set', function () {
+      var sets = ['l', 'u', 'd', 'p', 'lu', 'ld', 'lp', 'ud', 'up', 'dp', 'lud', 'lup', 'ldp', 'udp'];
+      sets.forEach(function (set) {
+        var pwd = rpg({length: 50, set: set});
+        var allowed = getAllowedCharacters(set);
+        pwd.split('').forEach(function (c) {
+          allowed.should.containEql(c);
+        });
+      });
     });
+    it('should throw error when set is not a string', function () {
+      var fn = function () {};
+      var sets = [0, 1, true, false, {}, [], null, fn];
+      sets.forEach(function (set) {
+        (function () {
+          rpg({set: set});
+        }).should.throw(/set/);
+      });
+    });
+    it('should throw error when set is not combination of l, u, d, p', function () {
+      var sets = ['', 'a', 'aludp', 'ludpb', 'lucdp'];
+      sets.forEach(function (set) {
+        (function () {
+          rpg({set: set});
+        }).should.throw(/set/);
+      });
+    });
+
   });
 
   describe('options.exclude', function () {
-    it.skip('', function () {
+    it('should be able to set excluded characters', function () {
+      var sets = ['l', 'u', 'd', 'p', 'lu', 'ld', 'lp', 'ud', 'up', 'dp', 'lud', 'lup', 'ldp', 'udp'];
+      var options = sets.map(function (set) {
+        var exclude = getAllowedCharacters(set);
+        return {set: set, exclude: exclude.substring(Math.random() * exclude.length, Math.random() * exclude.length)};
+      });
+      options.forEach(function (option) {
+        var pwd = rpg(option);
+        pwd.split('').forEach(function (c) {
+          option.exclude.should.not.containEql(c);
+        });
+      });
+    });
+    it('should throw error when exclude is not a string', function () {
+      var fn = function () {};
+      var excludes = [0, 1, true, false, {}, [], null, fn];
+      excludes.forEach(function (exclude) {
+        (function () {
+          rpg({exclude: exclude});
+        }).should.throw(/exclude/);
+      });
+    });
+    it('should throw error when allowed character set becomes empty', function () {
+      var sets = ['l', 'u', 'd', 'p', 'lu', 'ld', 'lp', 'ud', 'up', 'dp', 'lud', 'lup', 'ldp', 'udp'];
+      var options = sets.map(function (set) {
+        return {set: set, exclude: getAllowedCharacters(set)};
+      });
+      options.forEach(function (option) {
+        (function () {
+          rpg(option);
+        }).should.throw(/allowed/);
+      });
     });
   });
+
+  function getAllowedCharacters(set, exclude) {
+    if (!set) {
+      set = 'ludp';
+    }
+    if (!exclude) {
+      exclude = '';
+    }
+    var characters = {
+      lchars: 'abcdefghijklmnopqrstuvwxyz',
+      uchars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      digits: '0123456789',
+      punctuations: '`~@#$%^&*-_=+()[]{}<>,.;:!?|/\'"\\'
+    };
+    var allowed = '';
+    allowed += set.indexOf('l') > -1 ? characters.lchars : '';
+    allowed += set.indexOf('u') > -1 ? characters.uchars : '';
+    allowed += set.indexOf('d') > -1 ? characters.digits : '';
+    allowed += set.indexOf('p') > -1 ? characters.punctuations : '';
+    allowed = allowed.split('').filter(function (c) {
+      return exclude.indexOf(c) < 0;
+    }).join('');
+    return allowed;
+  }
 });
